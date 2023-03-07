b8bce7aeb63f5bd188837c2de260760b665f5e7d
Samuel Gopeh <sammygopeh@gmail.com>
2023-03-07 14:20:02 +0100

    unnecessary imports clean up

--- src/__tests__/integration_tests/data/country/country.repo.int.test.ts
+++ src/__tests__/integration_tests/data/country/country.repo.int.test.ts
@@ -1,38 +1,22 @@
-import { runAssociations } from "@data/associations";
 import { CountryRepo, Country } from "@data/country";
 import { CreateCountryDto } from "@logic/country";
-import {
-    countryObj,
-    countryJson,
-    countryData,
-    countryObjArray,
-    countryJsonArray,
-    countrySeeder,
-} from "src/__tests__/samples";
+import { countrySeeder } from "src/__tests__/samples";
 import { DBSetup } from "src/__tests__/test_utils";
 
-const modelContext = {
-    create: jest.fn(),
-    findByPk: jest.fn(),
-    findAll: jest.fn(),
-};
-
-const countryRepo = new CountryRepo(modelContext as unknown as typeof Country);
-
-const testCountryRepo = new CountryRepo(Country);
+const countryRepo = new CountryRepo(Country);
 
 DBSetup(countrySeeder);
 
 describe("Testing country repo", () => {
     describe("Testing create", () => {
         it("should return a country object", async () => {
-            const createMock = jest.spyOn(testCountryRepo, "create");
+            const createMock = jest.spyOn(countryRepo, "create");
             const data = new CreateCountryDto({
                 currencyCode: "NGN",
                 name: "Nigeria",
                 isoCode: "NGI",
             });
-            const country = await testCountryRepo.create(data);
+            const country = await countryRepo.create(data);
             expect(country).toBeDefined();
             expect(country.name).toBe(data.name);
             expect(country.isoCode).toBe(data.isoCode);
@@ -46,7 +30,7 @@ describe("Testing country repo", () => {
             it("should return the country object", async () => {
                 const existingCountry = await Country.findOne();
                 if (!existingCountry) throw new Error("Failed to seed countries");
-                const country = await testCountryRepo.getByCode(existingCountry.isoCode);
+                const country = await countryRepo.getByCode(existingCountry.isoCode);
                 if (!country) throw new Error("Country not found");
                 expect(country).toBeDefined();
                 expect(country.name).toBe(existingCountry.name);
@@ -57,7 +41,7 @@ describe("Testing country repo", () => {
 
         describe("Given the country doesn't exists", () => {
             it("should return null", async () => {
-                const country = await testCountryRepo.getByCode("MGM");
+                const country = await countryRepo.getByCode("MGM");
                 expect(country).toBe(null);
             });
         });
@@ -67,7 +51,7 @@ describe("Testing country repo", () => {
         it("should return an array of supported countries", async () => {
             const existingCountries = await Country.findAll();
             const existingJsons = existingCountries.map((cou) => cou.toJSON());
-            const countries = await testCountryRepo.getAll();
+            const countries = await countryRepo.getAll();
             expect(countries).toEqual(existingJsons);
         });
     });
